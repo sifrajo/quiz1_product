fe63ccaa0f9380886fb7bfb46df7feac800a83ee
sifrajo <jesseorlanda241102@gmail.com>
2023-04-11 12:47:59 +0700

    yield

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,51 +1,44 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/css/style.css">
-    <title>QUIZ1_PRODUCT</title>
-</head>
-<body>
-    <div class="products">
-        <h1 class="title">PRODUCT LIST</h1>
+@extends('layouts.main')
 
-        @foreach ($products as $product)
-            @if ($product["status"] === "R")
-                <div class="R">
-                    <h1>
-                        <a href="/products/{{ $product["slug"] }}">{{ $product["name"] }}</a>
-                    </h1>
-                    <h3>{{ $product["price"] }}</h3>
-                    <h3>{{ $product["status"] }}</h3>
-                    <p>{{ $product["desc"] }}</p>
-                </div>
+@section('title', 'Products')
 
-            @elseif ($product["status"] === "SR")
-                <div class="SR">
-                    <h1>
-                        <a href="/products/{{ $product["slug"] }}">{{ $product["name"] }}</a>
-                    </h1>
-                    <h3>{{ $product["price"] }}</h3>
-                    <h3>{{ $product["status"] }}</h3>
-                    <p>{{ $product["desc"] }}</p>
-                </div>
+{{-- @section('meta_description',{{ $products["desc"]}}->meta_description) --}}
 
-            @elseif ($product["status"] === "SSR")
-                <div class="SSR">
-                    <h1>
-                        <a href="/products/{{ $product["slug"] }}">{{ $product["name"] }}</a>
-                    </h1>
-                    <h3>{{ $product["price"] }}</h3>
-                    <h3>{{ $product["status"] }}</h3>
-                    <p>{{ $product["desc"] }}</p>
-                </div>
+@section('container')
+    <h1 class="title">PRODUCT LIST</h1>
 
-            @endif
-            <br>
-        @endforeach
-    </div>
+    @foreach ($products as $product)
+        @if ($product["status"] === "R")
+            <div class="R">
+                <h1>
+                    <a href="/products/{{ $product["slug"] }}">{{ $product["name"] }}</a>
+                </h1>
+                <h3>{{ $product["price"] }}</h3>
+                <h3>{{ $product["status"] }}</h3>
+                <p>{{ $product["desc"] }}</p>
+            </div>
 
-</body>
-</html>
+        @elseif ($product["status"] === "SR")
+            <div class="SR">
+                <h1>
+                    <a href="/products/{{ $product["slug"] }}">{{ $product["name"] }}</a>
+                </h1>
+                <h3>{{ $product["price"] }}</h3>
+                <h3>{{ $product["status"] }}</h3>
+                <p>{{ $product["desc"] }}</p>
+            </div>
+
+        @elseif ($product["status"] === "SSR")
+            <div class="SSR">
+                <h1>
+                    <a href="/products/{{ $product["slug"] }}">{{ $product["name"] }}</a>
+                </h1>
+                <h3>{{ $product["price"] }}</h3>
+                <h3>{{ $product["status"] }}</h3>
+                <p>{{ $product["desc"] }}</p>
+            </div>
+
+        @endif
+        <br>
+    @endforeach
+@stop
